fd43f7858bf57b6a0aa0eafe75471334c6c77662
oleg_start <109465472+morgun741@users.noreply.github.com>
2024-09-11 23:04:51 +0300

    task2.py

--- task2.py
+++ task2.py
@@ -1,28 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 df = pd.read_excel('C:/Users/user/Downloads/train.xlsx')
-
-
 print(df.head())
-
-
 plt.figure(figsize=(10, 6))
 plt.boxplot(df['Sales'], vert=False)
 plt.title('Коробочный сюжет для продаж')
 plt.xlabel('Продажи')
 plt.show()
-
-
 mean_sales = df['Sales'].mean()
 std_sales = df['Sales'].std()
-
-
 lower_bound = mean_sales - 3 * std_sales
 upper_bound = mean_sales + 3 * std_sales
-
-
 filtered_sales = df[(df['Sales'] >= lower_bound) & (df['Sales'] <= upper_bound)]
 
 
