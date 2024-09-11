2e6501f1a5f4958caf8f0d0c4080c6a46f051441
oleg_start <109465472+morgun741@users.noreply.github.com>
2024-09-11 23:20:12 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,5 +4,6 @@ df = pd.read_excel('C:/Users/user/Downloads/train.xlsx')
 df['Sale_group'] = pd.qcut(df['Sales'], q=3, labels=['Малые продажи', 'Средние продажи', 'Высокие продажи'])
 region_sales_group = df.groupby(['Region', 'Sale_group']).size().unstack()
 print(region_sales_group)
+
 most_profitable_group = df.groupby('Sale_group')['Sales'].sum().idxmax()
 print(f'Наиболее прибыльная группа продаж: {most_profitable_group}')
