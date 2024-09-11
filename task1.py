766a74979819c7281b72b3c2b6825316860409b0
oleg_start <109465472+morgun741@users.noreply.github.com>
2024-09-11 22:59:39 +0300

    task1.py

--- task1.py
+++ task1.py
@@ -1,23 +1,23 @@
 import pandas as pd
 
-# Загрузка данных
+
 df = pd.read_excel('C:/Users/user/Downloads/train.xlsx')
 
-# Преобразуем колонку с датами в формат datetime
+
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 
-# а) Наиболее часто покупаемые подгруппы товаров за всё время продаж
+
 top_subcategories_all_time = df['Sub-Category'].value_counts().head(4)
 
-# б) Наиболее часто покупаемые подгруппы товаров за последние два года
+
 latest_two_years = df[df['Order Date'] >= pd.Timestamp.now() - pd.DateOffset(years=2)]
 top_subcategories_last_two_years = latest_two_years['Sub-Category'].value_counts().head(4)
 
-# в) Наиболее часто покупаемые подгруппы товаров за последний год
+
 latest_one_year = df[df['Order Date'] >= pd.Timestamp.now() - pd.DateOffset(years=1)]
 top_subcategories_last_one_year = latest_one_year['Sub-Category'].value_counts().head(4)
 
-# Вывод результатов
+
 print("Наиболее часто покупаемые подгруппы за всё время:")
 print(top_subcategories_all_time)
 
