19738913a212b233a68f26bc1e601da2f85e0ff3
oleg_start <109465472+morgun741@users.noreply.github.com>
2024-09-11 20:49:34 +0300

    task1.py

--- task1.py
+++ task1.py
@@ -1,36 +1,28 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-#Загрузка данных
-data = pd.read_excel('/mnt/data/train.xlsx')
-data['Order Date'] = pd.to_datetime(data['Order Date'], format='%Y-%m-%d')
+# Загрузка данных
+df = pd.read_excel('C:/Users/user/Downloads/train.xlsx')
 
-#a) Наиболее часто покупаемые подгруппы за все время
-subcat_all_time = data['Sub-Category'].value_counts().nlargest(4)
+# Преобразуем колонку с датами в формат datetime
+df['Order Date'] = pd.to_datetime(df['Order Date'])
 
-#b) Наиболее часто покупаемые подгруппы за последние два года
-last_two_years = data[data['Order Date'] >= (data['Order Date'].max() - pd.DateOffset(years=2))]
-subcat_last_two_years = last_two_years['Sub-Category'].value_counts().nlargest(4)
+# а) Наиболее часто покупаемые подгруппы товаров за всё время продаж
+top_subcategories_all_time = df['Sub-Category'].value_counts().head(4)
 
-#c) Наиболее часто покупаемые подгруппы за последний год
-last_year = data[data['Order Date'] >= (data['Order Date'].max() - pd.DateOffset(years=1))]
-subcat_last_year = last_year['Sub-Category'].value_counts().nlargest(4)
+# б) Наиболее часто покупаемые подгруппы товаров за последние два года
+latest_two_years = df[df['Order Date'] >= pd.Timestamp.now() - pd.DateOffset(years=2)]
+top_subcategories_last_two_years = latest_two_years['Sub-Category'].value_counts().head(4)
 
-#Визуализация
-fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+# в) Наиболее часто покупаемые подгруппы товаров за последний год
+latest_one_year = df[df['Order Date'] >= pd.Timestamp.now() - pd.DateOffset(years=1)]
+top_subcategories_last_one_year = latest_one_year['Sub-Category'].value_counts().head(4)
 
-#Подгруппы за все время
-axes[0].bar(subcat_all_time.index, subcat_all_time.values, color='blue')
-axes[0].set_title('Часто покупаемые подгруппы (Все время)')
-axes[0].set_ylabel('Частота покупок')
+# Вывод результатов
+print("Наиболее часто покупаемые подгруппы за всё время:")
+print(top_subcategories_all_time)
 
-#Подгруппы за последние два года
-axes[1].bar(subcat_last_two_years.index, subcat_last_two_years.values, color='green')
-axes[1].set_title('Часто покупаемые подгруппы (Последние 2 года)')
+print("\nНаиболее часто покупаемые подгруппы за последние два года:")
+print(top_subcategories_last_two_years)
 
-#Подгруппы за последний год
-axes[2].bar(subcat_last_year.index, subcat_last_year.values, color='red')
-axes[2].set_title('Часто покупаемые подгруппы (Последний год)')
-
-plt.tight_layout()
-plt.show()
+print("\nНаиболее часто покупаемые подгруппы за последний год:")
+print(top_subcategories_last_one_year)
